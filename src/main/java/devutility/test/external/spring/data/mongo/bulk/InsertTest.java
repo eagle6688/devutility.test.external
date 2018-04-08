6878ca7f298a84f10af8290faaa9315167dc391a
Aldwin Su <fangyansu@163.com>
2018-04-09 00:10:17 +0800

    Update

--- src/main/java/devutility/test/external/spring/data/mongo/bulk/InsertTest.java
+++ src/main/java/devutility/test/external/spring/data/mongo/bulk/InsertTest.java
@@ -1,7 +1,5 @@
 package devutility.test.external.spring.data.mongo.bulk;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.BulkOperations;
@@ -14,13 +12,13 @@ import com.devutility.test.model.Student;
 import com.mongodb.bulk.BulkWriteResult;
 
 import devutility.internal.test.TestExecutor;
-import devutility.internal.util.RandomHelper;
+import devutility.test.external.data.StudentsData;
 import devutility.test.external.spring.data.mongo.BaseMongo;
 
 public class InsertTest extends BaseMongo {
 	@Override
 	public void run() {
-		List<Student> list = list();
+		List<Student> list = StudentsData.list();
 		BulkOperations bulkOperations = mongoOperations.bulkOps(BulkMode.UNORDERED, "Student");
 
 		for (Student item : list) {
@@ -43,24 +41,6 @@ public class InsertTest extends BaseMongo {
 		println(bulkWriteResult.getMatchedCount());
 		println(bulkWriteResult.getUpserts().size());
 		System.out.println(bulkWriteResult);
-	}
-
-	private List<Student> list() {
-		List<Student> list = new ArrayList<>(100);
-
-		for (int i = 0; i < 100; i++) {
-			int number = i + 1;
-
-			Student student = new Student();
-			student.setAge(RandomHelper.getNumber(20));
-			student.setBirthday(new Date());
-			student.setLocal(true);
-			student.setName(String.format("Name_%d", number));
-			student.setNumber(String.valueOf(number));
-			list.add(student);
-		}
-
-		return list;
 	}
 
 	public static void main(String[] args) {
