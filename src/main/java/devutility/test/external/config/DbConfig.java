a833f6cf782d9b1f4ab13efb9d23c15b5294ba3e
Aldwin Su <fangyansu@163.com>
2018-04-12 14:10:53 +0800

    Update

--- src/main/java/devutility/test/external/config/DbConfig.java
+++ src/main/java/devutility/test/external/config/DbConfig.java
@@ -1,12 +1,7 @@
 package devutility.test.external.config;
 
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import devutility.external.dao.mongodb.MongoDBHelper;
 import devutility.external.dao.redis.RedisHelper;
-import devutility.internal.dao.DbInstanceHelper;
 import devutility.internal.dao.RedisInstanceHelper;
-import devutility.internal.dao.models.DbInstance;
 import devutility.internal.dao.models.RedisInstance;
 
 public class DbConfig {
@@ -14,22 +9,6 @@ public class DbConfig {
 	 * Configuration for database
 	 */
 	public static final String CONFIG_NAME = "dbconfig.properties";
-
-	/**
-	 * MongoDbHolder
-	 */
-	private static class MongoDbHolder {
-		private static DbInstance dbInstance = DbInstanceHelper.getInstance(CONFIG_NAME, "mongodb");
-		public static MongoTemplate mongoTemplate = MongoDBHelper.mongoTemplate(dbInstance);
-	}
-
-	/**
-	 * mongoDb
-	 * @returnMongoTemplate
-	 */
-	public static MongoTemplate mongoDb() {
-		return MongoDbHolder.mongoTemplate;
-	}
 
 	private static class RedisHolder {
 		private static RedisInstance redisInstance = RedisInstanceHelper.getInstance(CONFIG_NAME, "redis");
