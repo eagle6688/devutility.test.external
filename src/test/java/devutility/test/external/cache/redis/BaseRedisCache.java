38f7f57660cd6ca75a6bdc302b58eeddc6d1dfb2
Aldwin Su <fangyansu@163.com>
2018-04-24 17:47:12 +0800

    Update

--- src/test/java/devutility/test/external/cache/redis/BaseRedisCache.java
+++
@@ -1,9 +0,0 @@
-package devutility.test.external.cache.redis;
-
-import devutility.external.dao.redis.RedisHelper;
-import devutility.internal.test.BaseTest;
-import devutility.test.external.config.DbConfig;
-
-public abstract class BaseRedisCache extends BaseTest {
-	protected RedisHelper redisHelper = DbConfig.redisHelper();
-}
