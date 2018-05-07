0736b7e814d310eb9b206072885b11dc71d4e916
Aldwin Su <fangyansu@163.com>
2018-05-07 20:30:57 +0800

    Degrade JDK to 1.8

--- src/test/java/devutility/test/external/config/MailConfig.java
+++ src/test/java/devutility/test/external/config/MailConfig.java
@@ -4,22 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import devutility.internal.util.PropertiesHelper;
+import devutility.internal.util.PropertiesUtils;
 
 public class MailConfig {
 	public static final String configFile = "email.properties";
 
 	public static Map<String, String> get() {
-		Properties properties = PropertiesHelper.getProperties(configFile);
+		Properties properties = PropertiesUtils.getProperties(configFile);
 
 		Map<String, String> map = new HashMap<String, String>();
-		map.put("host", PropertiesHelper.getProperty(properties, "host"));
-		map.put("port", PropertiesHelper.getProperty(properties, "port"));
-		map.put("userName", PropertiesHelper.getProperty(properties, "userName"));
-		map.put("password", PropertiesHelper.getProperty(properties, "password"));
-		map.put("fromEmail", PropertiesHelper.getProperty(properties, "fromEmail"));
-		map.put("toEmails", PropertiesHelper.getProperty(properties, "toEmails"));
-		map.put("copyEmails", PropertiesHelper.getProperty(properties, "copyEmails"));
+		map.put("host", PropertiesUtils.getProperty(properties, "host"));
+		map.put("port", PropertiesUtils.getProperty(properties, "port"));
+		map.put("userName", PropertiesUtils.getProperty(properties, "userName"));
+		map.put("password", PropertiesUtils.getProperty(properties, "password"));
+		map.put("fromEmail", PropertiesUtils.getProperty(properties, "fromEmail"));
+		map.put("toEmails", PropertiesUtils.getProperty(properties, "toEmails"));
+		map.put("copyEmails", PropertiesUtils.getProperty(properties, "copyEmails"));
 		return map;
 	}
 }
